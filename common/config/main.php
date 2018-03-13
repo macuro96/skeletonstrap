f94e444d17f0a8d767f276ad3ad35355832e6e66
Manuel Cuevas Rodriguez <manuelcuevasrod96@gmail.com>
2018-03-13 18:12:11 +0100

    Configuracion inicial aplicacion y docs

--- common/config/main.php
+++ common/config/main.php
@@ -4,10 +4,15 @@ return [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+    'language' => 'es-ES',
+    'name'     => 'Skeleton\'s Trap',
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
+        ],
+        'formatter' => [
+            'timeZone' => 'Europe/Madrid',
         ],
     ],
 ];

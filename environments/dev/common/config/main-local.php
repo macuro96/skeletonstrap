ba46012c55081d2f5dc7c00d5158582b4d7b7bca
Manuel Cuevas Rodriguez <manuelcuevasrod96@gmail.com>
2018-03-14 17:11:49 +0100

    Hoja de estilos common

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -16,5 +16,8 @@ return [
             // for the mailer to send real emails.
             'useFileTransport' => true,
         ],
+        'assetManager' => [
+            'forceCopy' => true
+        ]
     ],
 ];

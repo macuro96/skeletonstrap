1fdd2b43fa9e37327e4bd8965dd614f841a80910
Manuel Cuevas Rodriguez <manuelcuevasrod96@gmail.com>
2018-03-17 12:20:56 +0100

    Dividiendo hojas de estilo _directo.css y anadiendo estilos a marcadores

--- frontend/views/site/_directo.php
+++ frontend/views/site/_directo.php
@@ -4,6 +4,7 @@ use yii\helpers\Html;
 
 use common\components\CheckEnd;
 
+$this->registerCssFile('css/_directo.css');
 ?>
 <div class="row div-directo">
     <div class="col-md-12">
@@ -26,22 +27,52 @@ use common\components\CheckEnd;
         </div>
         <div class="row equipos">
             <div class="col-md-offset-3 col-md-2">
-                <div>
-                    <?= Html::img(CheckEnd::rutaRelativa() . 'images/logo.png', ['class' => 'img-responsive']) ?>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-4 col-md-12">
+                        <div class="container-img">
+                            <?= Html::img(CheckEnd::rutaRelativa() . 'images/logo.png', ['class' => 'img-responsive']) ?>
+                        </div>
+                        <div>
+                            <p>Skeleton's Trap</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 visible-sm guion">
+                        -
+                    </div>
+                    <div class="col-sm-4 visible-sm marcador-local">
+                        <marcadorlocal>0</marcadorlocal>
+                    </div>
                 </div>
-                <div>
-                    <p>Skeleton's Trap</p>
+                <div class="row hidden-sm">
+                    <div class="col-md-12">
+                        <marcadorlocal>0</marcadorlocal>
+                    </div>
                 </div>
             </div>
             <div class="col-md-2">
                 VS
             </div>
             <div class="col-md-2">
-                <div>
-                    <?= Html::img(CheckEnd::rutaRelativa() . 'images/Logo4K1.png', ['class' => 'img-responsive']) ?>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-4 col-md-12">
+                        <div class="container-img">
+                            <?= Html::img(CheckEnd::rutaRelativa() . 'images/Logo4K1.png', ['class' => 'img-responsive']) ?>
+                        </div>
+                        <div>
+                            <p>RoyaleconQueso</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 visible-sm guion">
+                        -
+                    </div>
+                    <div class="col-sm-4 visible-sm marcador-local">
+                        <marcadorlocal>0</marcadorlocal>
+                    </div>
                 </div>
-                <div>
-                    <p>RoyaleconQueso</p>
+                <div class="row hidden-sm">
+                    <div class="col-md-12">
+                        <marcadorlocal>0</marcadorlocal>
+                    </div>
                 </div>
             </div>
         </div>

54ab819307ea829c0fc548908ce9a4b56d7ce4be
Manuel Cuevas Rodriguez <manuelcuevasrod96@gmail.com>
2018-04-07 13:48:34 +0200

    Refactorizando componente de MostrarIndex a vistas parciales e index

--- frontend/views/site/_proximaPartida.php
+++ frontend/views/site/_proximaPartida.php
@@ -1,34 +1,21 @@
 <?php
 
-/* @var $detect       Detection\MobileDetect */
-/* @var $etiqueta     Etiqueta de partida */
-/* @var $nombreEvento Nombre del evento asociado a la partida */
+/* @var $detect Detection\MobileDetect */
 
 use yii\helpers\Html;
 use common\components\CheckEnd;
 use common\components\RedesSociales;
-use common\components\Twitch;
-
-$etiqueta     = mb_strtoupper($etiqueta);
-$nombreEvento = $nombreEvento ?? '';
 ?>
-<div class="row proxima-partida directo">
+<div class="row proxima-partida">
     <div class="col-md-offset-2 col-md-8">
         <div class="row text-centered div-titulo">
             <div class="col-md-12">
-                <h2 class="titulo-proxima-partida titulo-directo">EN DIRECTO <i class="fa fa-circle"></i></h2>
+                <h2 class="titulo-proxima-partida">PRÓXIMA PARTIDA <span class="glyphicon glyphicon-refresh btn" aria-hidden="true"></span></h2>
             </div>
         </div>
         <div class="row text-centered div-subtitulo">
-            <div class="col-md-12">
-                <!--<a href="#">-->
-                    <h3 class="subtitulo-proxima-partida">- <etiqueta><?= $etiqueta ?></etiqueta><?= $nombreEvento != null ? ": $nombreEvento " : '' ?> -</h3>
-                <!--</a>-->
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-12 centrar div-directo">
-                <?= Twitch::directo() ?>
+            <div class="col-md-12 redes">
+                <h3 class="subtitulo-proxima-partida"><span class="glyphicon glyphicon-time" aria-hidden="true"></span> <hora>14:00 PM</hora> <a href="https://www.twitch.tv/skeletonstraptv" class="twitch"><i class="fa fa-twitch"></i></a></h3>
             </div>
         </div>
         <div class="row">
@@ -41,7 +28,6 @@ $nombreEvento = $nombreEvento ?? '';
                 <div class="row nombres-equipos">
                     <div class="col-md-12 equipo">
                         <h3>Skeleton's Trap</h3>
-                        <h3><marcador>0</marcador></h3>
                     </div>
                 </div>
             </div>
@@ -61,7 +47,6 @@ $nombreEvento = $nombreEvento ?? '';
                 <div class="row nombres-equipos">
                     <div class="col-md-12 equipo-enemigo">
                         <h3>TeamQueso</h3>
-                        <h3><marcador>0</marcador></h3>
                     </div>
                 </div>
             </div>
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,11 +1,147 @@
 <?php
 
 /* @var $this yii\web\View */
+/* @var $detect Detection\MobileDetect */
 
-$this->title = 'My Yii Application';
+use yii\helpers\Html;
+use yii\web\View;
+
+use common\components\Twitch;
+use common\components\CheckEnd;
+use common\components\RedesSociales;
+
+$this->title = 'Inicio';
+
+$this->registerCssFile('css/_index.css');
+$this->registerJsFile('js/_index.js', ['position' => View::POS_END,
+                                       'depends'  => [\yii\web\JqueryAsset::className()]]);
 ?>
-<?=
-    $this->render('_directo', [
-        'detect' => $detect
-    ]);
-?>
+
+<div class="row cabecera-inicio">
+    <div class="col-lg-offset-3 col-lg-6">
+        <div class="img-centered logo-inicio">
+            <?= Html::img(CheckEnd::rutaRelativa() . 'images/logo.png', ['class' => 'img-responsive']) ?>
+        </div>
+
+        <p class="titulo-inicio">
+            Trappero, bienvenido a la web del equipo <b>"Skeleton's Trap"</b>
+        </p>
+
+        <p>
+            Ésta es una web en la que encontrarás información sobre: integrantes del equipo, estadísticas, torneos y para establecer un contacto para poder unirte o luchar contra nosotros...
+        </p>
+    </div>
+</div>
+
+<evento-partida>
+    <?= $this->render('_proximaPartida', [
+        'detect' => $detect,
+    ]) ?>
+</evento-partida>
+
+<div class="row seccion">
+    <div class="col-lg-12">
+        <div class="titulo-seccion"><h2>Sobre nosotros</h2></div>
+
+        <div class="contenido-seccion">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="img-centered">
+                        <?= Html::img(CheckEnd::rutaRelativa() . 'images/clash-royale-logo.png', ['class' => 'img-responsive']) ?>
+                    </div>
+                </div>
+                <div class="col-lg-8">
+                    <p>
+                        Sed vel est quis risus imperdiet eleifend. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In et mi rutrum, laoreet lorem id, lobortis nunc. Sed vehicula libero quam, vel porta sapien tincidunt vitae. Sed congue ligula ac neque suscipit tempor non placerat diam.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row seccion">
+    <div class="col-lg-12">
+        <div class="titulo-seccion" id="ultimos-encuentros">
+            <h2>Últimos encuentros <span class="glyphicon glyphicon-refresh btn" aria-hidden="true"></span></h2>
+            <h6>Última actualización hace: <tiempo>1 min</tiempo>...</h6>
+        </div>
+
+        <div class="contenido-seccion">
+            <div class="row encuentro">
+                <div class="col-lg-12">
+                    <table class="table table-stripped">
+                        <thead>
+                            <tr>
+                                <th class="hidden-logo">
+                                    Logo
+                                </th>
+                                <th>
+                                    Equipo
+                                </th>
+                                <th>
+                                    Estadio
+                                </th>
+                                <th>
+                                    Marcador
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="hidden-logo">
+                                    <?= Html::img(CheckEnd::rutaRelativa() . 'images/logo.png', ['class' => 'img-responsive']) ?>
+                                </td>
+                                <td>Skeleton's Trap</td>
+                                <td>Stadium Trap</td>
+                                <td class="marcador">0 - 0</td>
+                            </tr>
+                            <tr>
+                                <td class="hidden-logo">
+                                    <?= Html::img(CheckEnd::rutaRelativa() . 'images/Logo4K1.png', ['class' => 'img-responsive']) ?>
+                                </td>
+                                <td>TeamQueso</td>
+                                <td>Stadium Trap</td>
+                                <td class="marcador">3 - 0</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row seccion">
+    <div class="col-lg-12">
+        <div class="titulo-seccion"><h2>Mejores partidas</h2></div>
+
+        <div class="contenido-seccion">
+            <div class="row">
+                <div class="col-lg-12 centrar">
+                    <?= Twitch::coleccion('8NTmRZP42xSlkg') ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12 centrar">
+                    <h2><?= Html::a('¿Te atréves a jugar <b>contra nosotros</b>?', ['/site/lucha'], ['class' => 'a-none']) ?></h2>
+                    <div class="redes-sociales">
+                        <?= RedesSociales::twitter('https://skeletons-trap.herokuapp.com/ ¿Te atréves a jugar contra nosotros? Somos Skeleton\'s Trap') ?>
+                        <?= RedesSociales::whatsapp($detect, 'https://skeletons-trap.herokuapp.com/ ¿Te atréves a jugar contra nosotros? Somos Skeleton\'s Trap') ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row seccion">
+    <div class="col-lg-12">
+        <div class="titulo-seccion"><h2>Novedades</h2></div>
+
+        <div class="contenido-seccion">
+            <div class="row">
+                <div class="col-lg-12 centrar">
+                    <?= RedesSociales::timelineTwitter() ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

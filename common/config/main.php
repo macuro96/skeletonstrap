ec39be36f2e7b0f941d533c85c5215b37a9bda9b
Manuel Cuevas Rodriguez <manuelcuevasrod96@gmail.com>
2018-04-09 19:38:09 +0200

    Url bonitas y enlaces en formulario de login

--- common/config/main.php
+++ common/config/main.php
@@ -15,5 +15,15 @@ return [
         'formatter' => [
             'timeZone' => 'Europe/Madrid',
         ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+                'defaultRoute' => '/site/index',
+            ],
+        ],
     ],
 ];
--- common/views/site/LoginView.php
+++ common/views/site/LoginView.php
@@ -3,8 +3,11 @@
 /* @var $form yii\bootstrap\ActiveForm */
 /* @var $model \common\models\LoginForm */
 
+use yii\helpers\Url;
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
+
+use common\components\CheckEnd;
 ?>
 
 <div class="site-login">
@@ -12,7 +15,7 @@ use yii\bootstrap\ActiveForm;
         <div class="col-lg-offset-4 col-lg-4">
             <h1><?= Html::encode($this->title) ?></h1>
 
-            <p>Para solicitar una cuenta primero debe aceptar las normas del equipo y rellenar un formulario, que se puede acceder a través de este enlace.</p>
+            <p>Para solicitar una cuenta primero debe aceptar las normas del equipo y rellenar un formulario, que se puede acceder a través de este <?= Html::a('enlace', '/site/unete') ?>.</p>
 
             <div class="row">
                 <div class="col-lg-12">
@@ -22,10 +25,16 @@ use yii\bootstrap\ActiveForm;
 
                         <?= $form->field($model, 'password')->passwordInput() ?>
 
+                        <?php if (CheckEnd::isFrontEnd()) : ?>
+                            <?= Html::a('Soy un administrador', '/admin/site/login') ?>
+                        <?php else : ?>
+                            <?= Html::a('Soy un jugador', '/site/login') ?>
+                        <?php endif; ?>
+
                         <?= $form->field($model, 'rememberMe')->checkbox() ?>
 
                         <div class="form-group">
-                            <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                            <?= Html::submitButton('Entrar', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
                         </div>
 
                     <?php ActiveForm::end(); ?>

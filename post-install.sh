8c52261b10f27e10e2ee585465fbbca0622f99ae
Manuel Cuevas Rodriguez <manuelcuevasrod96@gmail.com>
2018-04-17 18:38:21 +0200

    Eliminando migrate/create de post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -3,7 +3,6 @@ if [ -n "$DYNO" ]
 then
     php init --env=Production --overwrite=All
     ./yii migrate --migrationPath=@yii/rbac/migrations --interactive 0
-    ./yii migrate/create roles --interactive 0
     ./yii migrate/down --interactive 0
     ./yii migrate/up --interactive 0
 fi

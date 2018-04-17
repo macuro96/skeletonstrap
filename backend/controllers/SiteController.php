a482196d30d09de791c7f0254389bc143b428e54
Manuel Cuevas Rodriguez <manuelcuevasrod96@gmail.com>
2018-04-17 20:49:18 +0200

    Modificando redireccion de login en backend

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -78,7 +78,7 @@ class SiteController extends Controller
 
         $model = new LoginForm();
         if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            return $this->goBack();
+            return $this->goHome();
         } else {
             $model->password = '';
 

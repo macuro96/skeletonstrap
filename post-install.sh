12212feed1c55c4e0b2c6ced9e758b89fbfad7ac
Manuel Cuevas Rodriguez <manuelcuevasrod96@gmail.com>
2018-04-17 18:31:18 +0200

    Anadiendo migrate/down a post-install

--- post-install.sh
+++ post-install.sh
@@ -4,5 +4,6 @@ then
     php init --env=Production --overwrite=All
     ./yii migrate --migrationPath=@yii/rbac/migrations --interactive 0
     ./yii migrate/create roles --interactive 0
+    ./yii migrate/down --interactive 0
     ./yii migrate/up --interactive 0
 fi

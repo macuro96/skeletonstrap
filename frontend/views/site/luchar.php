b92d821873c6bb2269a4a56c20e602ea0561f2f9
Manuel Cuevas Rodriguez <manuelcuevasrod96@gmail.com>
2018-06-07 00:31:37 +0200

    Mejorando estilos de la vista de luchar

--- frontend/views/site/luchar.php
+++ frontend/views/site/luchar.php
@@ -7,15 +7,19 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
-$this->title = 'Solicitud de lucha';
+use common\assets\CommonFormNuevoUsuario;
 
+$this->title = 'Lucha';
+$this->params['breadcrumbs'][] = $this->title;
+
+CommonFormNuevoUsuario::register($this);
 ?>
 <div class="row seccion usuarios-form">
     <div class="col-lg-12">
-        <h2>¡Quiero luchar contra Skeleton's Trap!</h2>
+        <div class="titulo-seccion"><h2>¡Quiero luchar contra Skeleton's Trap!</h2></div>
 
-        <div>
-            <ul>
+        <div class="contenido-seccion">
+            <ul class="pasos">
                 <li>
                     Este formulario generará una solicitud al administrador. Si el administrador acepta, le enviará un correo con más
                     información.
